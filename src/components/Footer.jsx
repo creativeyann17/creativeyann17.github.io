77c78ac99ab21caf5df03a605a2351e36064877d
Yann MARCOU <creativeyann17@gmail.com>
2021-03-17 16:55:43 -0400

    better css footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,18 +11,14 @@ const Footer = ({ className }) => {
     <footer className={cx('footer', className)}>
       <Container className="py-5">
         <Row noGutters>
-          <div className="mb-3">
-            {renderExternalLinkByUrlAndLabel(ROUTES.EXTERNALS.GITHUB_TOS, 'Terms of use', 'ml-3')}
-            {renderExternalLinkByUrlAndLabel(
-              ROUTES.EXTERNALS.GITHUB_PRIVACY,
-              'Privacy policy',
-              'ml-3'
-            )}
+          <div className="mb-3 px-3">
+            {renderExternalLinkByUrlAndLabel(ROUTES.EXTERNALS.GITHUB_TOS, 'Terms of use', 'mr-3')}
+            {renderExternalLinkByUrlAndLabel(ROUTES.EXTERNALS.GITHUB_PRIVACY, 'Privacy policy')}
           </div>
           <div className="mb-3">
-            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.GITHUB, '/github128.png', 'ml-3')}
-            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.LINKEDIN, '/linkedin128.png', 'ml-3')}
-            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.TWITTER, '/twitter128.png', 'ml-3')}
+            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.GITHUB, '/github128.png', 'mr-3')}
+            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.LINKEDIN, '/linkedin128.png', 'mr-3')}
+            {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.TWITTER, '/twitter128.png')}
           </div>
         </Row>
         <Row noGutters>
