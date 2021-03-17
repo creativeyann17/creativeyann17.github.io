779eae6e233c0a4a994352f945fd330140a30f4c
Yann MARCOU <creativeyann17@gmail.com>
2021-03-17 16:23:19 -0400

    fix GA_TRACKING_ID

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -5,17 +5,20 @@ import { debug } from '../utils';
 import { PROD, GA_TRACKING_ID } from '../constants';
 
 const Analytics = ({ children }) => {
+  const renderDefault = () => <div>{children}</div>;
+
   const location = useLocation();
   if (PROD) {
     if (GA_TRACKING_ID) {
       return <ReactRouterGa id={GA_TRACKING_ID}>{children}</ReactRouterGa>;
     } else {
-      console.error('Missing GA_TRACKING_ID env. variable');
+      console.error('Analytics is disabled'); // don't want to give more details
+      return renderDefault();
     }
   } else {
     debug(`GA_TRACKING_ID: ${GA_TRACKING_ID}`);
     debug(`Analytics current path: ${location.pathname}`);
-    return <div>{children}</div>;
+    return renderDefault();
   }
 };
 
