caec8fd9a74d891624a91c82bded39e89fbb58cd
Yann MARCOU <creativeyann17@gmail.com>
2021-03-17 11:38:11 -0400

    fix Jest + 404 article

--- src/components/Markdown/CodeBlock.jsx
+++ src/components/Markdown/CodeBlock.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 // https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/AVAILABLE_STYLES_PRISM.MD
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 // My peak: coldarkDark coldarkCold okaidia tomorrow vscDarkPlus vs nord materialOceanic materialDark dracula
 const CodeBlock = ({ language, value }) => {
   return (
