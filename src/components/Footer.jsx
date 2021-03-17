3227f3ca69d240523d728465a5619353d2e8b584
Yann MARCOU <creativeyann17@gmail.com>
2021-03-17 14:16:41 -0400

    better CSS footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import cx from 'classnames';
 import { ROUTES } from '../constants';
 import { renderExternalLinkByUrlAndLabel, renderExternalLinkByUrlAndIcon } from '../utils';
@@ -10,22 +10,24 @@ const Footer = ({ className }) => {
   return (
     <footer className={cx('footer', className)}>
       <Container className="py-5">
-        <Col>
-          <Row className="mb-3">
+        <Row noGutters>
+          <div className="mb-3">
             {renderExternalLinkByUrlAndLabel(ROUTES.EXTERNALS.GITHUB_TOS, 'Terms of use', 'ml-3')}
             {renderExternalLinkByUrlAndLabel(
               ROUTES.EXTERNALS.GITHUB_PRIVACY,
               'Privacy policy',
               'ml-3'
             )}
+          </div>
+          <div className="mb-3">
             {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.GITHUB, '/github128.png', 'ml-3')}
             {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.LINKEDIN, '/linkedin128.png', 'ml-3')}
             {renderExternalLinkByUrlAndIcon(ROUTES.EXTERNALS.TWITTER, '/twitter128.png', 'ml-3')}
-          </Row>
-          <Row>
-            <p className="footer-copyright">© {currentYear} Yann MARCOU</p>
-          </Row>
-        </Col>
+          </div>
+        </Row>
+        <Row noGutters>
+          <p className="footer-copyright">© {currentYear} Yann MARCOU</p>
+        </Row>
       </Container>
     </footer>
   );
