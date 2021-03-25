06697d191bea267a8c414d1643caaf49976dbf85
creativeyann17 <35352649+creativeyann17@users.noreply.github.com>
2021-03-25 05:53:15 +0000

    Deploying to gh-pages from @ creativeyann17/creativeyann17.github.io@101143f38e64893e2c85536af72dbe068061013a 🚀

--- static/js/4.a4af72c3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcreativeyann17.github.io"]=this["webpackJsonpcreativeyann17.github.io"]||[]).push([[4],{772:function(e,t,c){"use strict";c.r(t);c(1);var i=c(748),n=c(30),a=c(157),s=c(87),b=c.n(s),r=c(50),j=c(46),u=c(2);t.default=Object(n.b)((function(e){return{articles:Object(j.a)(e)}}))((function(){return Object(u.jsxs)(i.a,{className:"page page-about",children:[Object(u.jsx)(a.a,{children:Object(u.jsx)("title",{children:"About"})}),Object(u.jsx)(b.a,{children:Object(u.jsx)(r.i,{})})]})}))}}]);
-//# sourceMappingURL=4.a4af72c3.chunk.js.map
