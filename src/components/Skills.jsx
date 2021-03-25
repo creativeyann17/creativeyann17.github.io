7d3b94b8d9d3e88d31ec2eee9bcdac8e77b89b92
Yann MARCOU <creativeyann17@gmail.com>
2021-03-25 02:15:35 -0400

    update README + skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,6 +5,8 @@ import { FiDatabase } from 'react-icons/fi';
 import { BiMicrochip, BiCode } from 'react-icons/bi';
 import { GiSkills } from 'react-icons/gi';
 import { BsCloud } from 'react-icons/bs';
+import { RiTempColdLine } from 'react-icons/ri';
+import { SiJavascript } from 'react-icons/si';
 import cx from 'classnames';
 import { ROUTES } from '../constants';
 import { renderExternalLinkByUrlAndIcon } from '../utils';
@@ -45,45 +47,52 @@ const Skills = ({ className }) => {
         </p>
         <div className="skills-item mb-3">
           <span>
-            <AiOutlineCodeSandbox />
-            <b>Frameworks: </b>
+            <SiJavascript />
+            <b>Front-end: </b>
           </span>
-          <span>Spring-boot | Micronaut | React.js | Bootstrap | Qt</span>
+          <span>React.js | GraphQl | Redux | Saga | Hooks | Bootstrap</span>
+        </div>
+        <div className="skills-item mb-3">
+          <span>
+            <AiOutlineCodeSandbox />
+            <b>Back-end: </b>
+          </span>
+          <span>Spring-boot | Micronaut | OpenAPI/Swagger</span>
         </div>
         <div className="skills-item mb-3">
           <span>
             <BiCode />
             <b>Languages: </b>
           </span>
-          <span>Java | JavaScript | HTML/CSS | C# | C/C++</span>
+          <span>Java | JavaScript | HTML/CSS/SCSS</span>
         </div>
         <div className="skills-item mb-3">
           <span>
             <AiOutlineCode />
             <b>Tools: </b>{' '}
           </span>
-          <span>Intellij | VSCode | Github | Bitbucket | npm | yarn | maven | bash</span>
+          <span>IntelliJ | VSCode | Github | Atlassian | npm | yarn | maven | bash</span>
         </div>
         <div className="skills-item mb-3">
           <span>
             <FiDatabase />
             <b>Databases: </b>{' '}
           </span>
-          <span>MongoDB | Oracle | PostgreSQL | H2 | Neo4j</span>
+          <span>MongoDB | H2 | PostgreSQL | Oracle</span>
         </div>
         <div className="skills-item mb-3">
           <span>
             <BsCloud />
             <b>CI / CD / Cloud: </b>{' '}
           </span>
-          <span>Jenkins | Circle-ci | Heroku | Openshift</span>
+          <span>Jenkins | Circle-ci | OpenShift | Heroku</span>
         </div>
         <div className="skills-item mb-3">
           <span>
             <BiMicrochip />
             <b>Embedded: </b>{' '}
           </span>
-          <span>Arduino | Raspberry-pi | Teensy-usb</span>
+          <span>Arduino | Raspberry-pi</span>
         </div>
         <div className="skills-item mb-3">
           <span>
@@ -91,6 +100,14 @@ const Skills = ({ className }) => {
             <b>Extra skills: </b>{' '}
           </span>
           <span>Tech lead | Shopify | Gimp | Blender | Agile methods</span>
+        </div>
+        <hr />
+        <div className="skills-item mb-3">
+          <span>
+            <RiTempColdLine />
+            <b>In need of a refresh: </b>{' '}
+          </span>
+          <span>C/C++ | C# | Qt | Swing | Neo4j | Play Framework 1.x</span>
         </div>
       </Col>
     </Row>
