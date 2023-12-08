e635e1649d79d5f0fc7c0d04dd697cce10f171ab
creativeyann17 <35352649+creativeyann17@users.noreply.github.com>
2023-12-08 23:20:58 +0000

    Deploying to gh-pages from @ creativeyann17/creativeyann17.github.io@2ff3ea97d35fcea9ce5e61417af7239d0fa54c23 🚀

---
+++ articles/article12.md
@@ -0,0 +1,1 @@
+Please check the github repository [README.md](https://github.com/creativeyann17/cy17-starter-react-app) for more details.
--- static/js/7.ded8c63d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcreativeyann17.github.io"]=this["webpackJsonpcreativeyann17.github.io"]||[]).push([[7],{881:function(e,t,a){"use strict";a.r(t);a(1);var n=a(855),c=a(872),i=a(25),s=a(197),r=a(2);t.default=function(e){e.articles;var t=Object(i.g)();return Object(r.jsxs)(n.a,{className:"page page-not-found ",children:[Object(r.jsx)(s.a,{children:Object(r.jsx)("title",{children:"404"})}),Object(r.jsxs)(c.a,{variant:"warning",className:"mt-3",children:[Object(r.jsx)("b",{children:"Page not found:"})," ",t.pathname]})]})}}}]);
-//# sourceMappingURL=7.ded8c63d.chunk.js.map
